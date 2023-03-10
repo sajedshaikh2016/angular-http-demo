ed8f877d5ee86fa48a2bf02d57b7d246acdadcb1
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-03-10 11:40:55 +0530

    Http Client implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  public isLoading: boolean = false;
+  public url: string = "https://api.github.com/users";
+  public users: any = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers() {
+    this.isLoading = true;
+    this.http.get(this.url).subscribe(response => {
+      console.log(response);
+      setTimeout(() => {
+        this.isLoading = false;
+        this.users = response;
+      }, 3000);
+
+    }, error => {
+      console.log(error);
+      this.isLoading = false;
+    }, () => {
+      console.log("Completed");
+    });
+  }
+
+  public goToGithub(url: string) {
+    window.open(url, '_blank');
+  }
 }
